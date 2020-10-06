5f13983753a60962d2b9297daf1c916254546422
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-06 13:18:45 +0100

    Fix Avg margin

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -3,7 +3,7 @@
         <span class="font-semibold text-xl lg:text-base avg_day">...</span>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Avg. Day</span>
     </div>
-    <div class="w-1/2 card mr-2 text-center relative">
+    <div class="w-1/2 card text-center relative">
         <span class="font-semibold text-xl lg:text-base avg_month">...</span>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Avg. Month</span>
     </div>  
