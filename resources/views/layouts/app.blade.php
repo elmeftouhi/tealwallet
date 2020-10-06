e73702a8ec1b2b2d741020ce52726ea14dc6a5ab
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-06 13:07:20 +0100

    Fix Avg Error 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css?v=1.0.2') }}" rel="stylesheet" type="text/css">
     <script src="{{ asset('js/app.js?v=1.0.2') }}" defer></script>
     
     <script src="{{ asset('js/jquery-ui.js') }}" defer></script>
