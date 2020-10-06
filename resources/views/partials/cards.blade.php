0642d2a864377266231ff72d8aaed139979fe9cb
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-06 16:29:30 +0100

    Fix Cards

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,10 +1,21 @@
-<div class="flex justify-between items-center px-3 py-3 mt-6 text-sm text-teal-600">
-    <div class="w-1/2 card mr-2 text-center relative">
-        <span class="font-semibold text-xl lg:text-base avg_day">...</span>
-        <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Avg. Day</span>
+<div class="flex justify-between items-center px-3 mt-8 text-sm text-teal-600">
+    <div class="w-1/2 card mr-2 text-center relative mb-1 pt-4 pb-8">
+        <span class="font-semibold text-xl lg:text-base sum_today">...</span>
+        <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 12px">
+            Avg : 
+            <span class="avg_day">...</span>
+            /Day
+        </div>
+        <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Today</span>
+
     </div>
-    <div class="w-1/2 card text-center relative">
-        <span class="font-semibold text-xl lg:text-base avg_month">...</span>
-        <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Avg. Month</span>
+    <div class="w-1/2 card text-center relative mb-1 pt-4 pb-8">
+        <span class="font-semibold text-xl lg:text-base sum_month">...</span>
+        <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 12px">
+            Avg : 
+            <span class="avg_month">...</span>
+            /Month
+        </div>
+        <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">This Month</span>
     </div>  
 </div>
