6d9fe6e14e7af7c4b809dbe7721d3275f31e5aac
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-07 17:24:51 +0100

    Expenses groupped by day and colors

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -34,25 +34,33 @@
             <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative mytable">
                 <thead>
                     <tr class="sticky top-0 border-b border-gray-300 bg-gray-100 items-center text-sm">
-                        <th class="py-4 px-2 pl-3 text-left w-6"><input type="checkbox" name="" id=""></th>
-                        <th class="text-left">Date</th>
+                        <th class="text-center">Date</th>
                         <th class="py-4 text-left">Details</th>
                         <th>Amount</th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                        $day = 0;
+                        $bg = "";
+                    ?>
                     @forelse ($expenses as $expense)
-                    <tr class="sticky top-0 border-b border-gray-300 items-center text-xs hover:bg-green-100">
-                        <td class="py-4 px-2 pl-3"><input type="checkbox" name="" id=""></td>
-                        <td class="w-12 text-left pr-2">{{ \Carbon\Carbon::parse($expense->expense_date)->format('d M') }} </td>
-                        <td>{{ $expense->description }}</td>
-                        <td class="w-16 px-1 text-right text-teal-800 font-bold">@money( $expense->amount )</td>
-                        <td class="w-12">
-                            <a href="{{route('expense.edit', ['expense'=>$expense->id])}}" class="flex-shrink-0 boder text-gray-600 py-2 px-3 focus:outline-none active:text-gray-800 active:shadow-inner hover:text-gray-700">
-                                <i class="fas fa-edit"></i>
-                            </a>                            
-                        </td>
+                        <?php
+                            if( $day != \Carbon\Carbon::parse($expense->expense_date)->format('d') )
+                                $bg = ($bg === "")? "bg-gray-200":"";
+                            
+                            $day = \Carbon\Carbon::parse($expense->expense_date)->format('d')
+                        ?>
+                        <tr class="sticky top-0 {{ $bg }} border-b border-gray-300 items-center text-xs hover:bg-green-100">
+                            <td class="w-12 text-center py-2">{{ \Carbon\Carbon::parse($expense->expense_date)->format('d M') }} </td>
+                            <td>{{ $expense->description }}</td>
+                            <td class="w-16 px-1 text-right text-teal-800 font-bold">@money( $expense->amount )</td>
+                            <td class="w-12">
+                                <a href="{{route('expense.edit', ['expense'=>$expense->id])}}" class="flex-shrink-0 boder text-gray-600 py-2 px-3 focus:outline-none active:text-gray-800 active:shadow-inner hover:text-gray-700">
+                                    <i class="fas fa-edit"></i>
+                                </a>                            
+                            </td>
                         </tr>
                     @empty
                     <tr>
