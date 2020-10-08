820505fc485d64682690b3c8f90f30c8b8bb853d
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-08 11:53:38 +0100

    Add link to Dashbord Card

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-between items-center px-3 mt-8 text-sm text-teal-600">
-    <div class="w-1/2 card mr-2 text-center relative mb-1 pt-4 pb-8">
+    <a href="{{ route('expense.index')}}" class="w-1/2 card mr-2 text-center relative mb-1 pt-4 pb-8">
         <span class="font-semibold text-xl lg:text-base sum_today">...</span>
         <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 12px">
             Avg : 
@@ -8,8 +8,8 @@
         </div>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Today</span>
 
-    </div>
-    <div class="w-1/2 card text-center relative mb-1 pt-4 pb-8">
+    </a>
+    <a href="{{ route('expense.index')}}" class="w-1/2 card text-center relative mb-1 pt-4 pb-8">
         <span class="font-semibold text-xl lg:text-base sum_month">...</span>
         <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 12px">
             Avg : 
@@ -17,5 +17,5 @@
             /Month
         </div>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">This Month</span>
-    </div>  
+    </a>  
 </div>
