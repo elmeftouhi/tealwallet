cbf393180940a7787bdd6f342be90407288736b5
Yassine <yassine.e@flowdesignssolutions.com>
2020-10-12 16:03:16 +0100

    Avg size

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,9 +1,9 @@
 <div class="flex justify-between items-center px-3 mt-8 text-sm text-teal-600">
     <a href="{{ route('expense.index')}}" class="w-1/2 card mr-2 text-center relative mb-1 pt-4 pb-8">
         <span class="font-semibold text-xl lg:text-base sum_today">...</span>
-        <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 12px">
+        <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 10px">
             Avg : 
-            <span class="avg_day">...</span>
+            <span class="avg_day font-bold">...</span>
             /Day
         </div>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">Today</span>
@@ -13,7 +13,7 @@
         <span class="font-semibold text-xl lg:text-base sum_month">...</span>
         <div class="text-center text-red-600 absolute bottom-0 right-0 mr-2 mb-2" style="font-size: 10px">
             Avg : 
-            <span class="avg_month">...</span>
+            <span class="avg_month font-bold">...</span>
             /Month
         </div>
         <span class="absolute top-0 left-0 -mt-3 bg-teal-600 text-teal-100 rounded px-2 text-xs">This Month</span>
